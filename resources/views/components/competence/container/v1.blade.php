8a11e4d1dbdea4c8878756a18fa05870b6681457
Nicolas R. Lopes <nicolasrlopes48@gmail.com>
2025-06-15 11:57:47 -0300

    feat: add competence container componente

---
+++ resources/views/components/competence/container/v1.blade.php
@@ -0,0 +1,8 @@
+<article class="flex flex-col gap-y-6 lg:gap-y-12">
+    <h2 class="lg:text-[2rem] font-medium leading-none px-4 lg:px-8">
+        {{ $title }}
+    </h2>
+    <div class="flex flex-col gap-y-10">
+        {{ $content }}
+    </div>
+</article>

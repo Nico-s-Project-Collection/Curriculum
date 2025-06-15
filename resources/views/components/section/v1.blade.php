63cc67ac249c2263b543f9849799c76a6d484dc5
Nicolas R. Lopes <nicolasrlopes48@gmail.com>
2025-06-15 11:48:18 -0300

    feat: add section component

---
+++ resources/views/components/section/v1.blade.php
@@ -0,0 +1,8 @@
+<section class="flex flex-col gap-y-8 lg:gap-y-16">
+    <h1 class="text-2xl lg:text-[3rem] font-semibold leading-none px-4 lg:px-8">
+        {{ $title }}
+    </h1>
+    <div class="flex flex-col gap-y-8 lg:gap-y-16">
+        {{ $content }}
+    </div>
+</section>

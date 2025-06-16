5ac11220120b3b3333c812c596988d0bc1fbcbad
Nicolas R. Lopes <nicolasrlopes48@gmail.com>
2025-06-16 19:33:19 -0300

    feat: add competence section in home

--- resources/views/pages/home/show/v1.blade.php
+++ resources/views/pages/home/show/v1.blade.php
@@ -1,4 +1,12 @@
 <x-html>
     <x-header.v1 />
-    <x-main.v1></x-main.v1>
+    <x-main.v1>
+        <x-section.v1>
+            <x-slot name="title">
+                Competências
+            </x-slot>
+            <x-slot name="content">
+            </x-slot>
+        </x-section.v1>
+    </x-main.v1>
 </x-html>

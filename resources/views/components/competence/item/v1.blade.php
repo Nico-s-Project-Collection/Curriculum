e5dccf18ef1f70466a1709fa17732b9ad8bdad8e
Nicolas R. Lopes <nicolasrlopes48@gmail.com>
2025-06-16 19:26:35 -0300

    feat: add competence item component

---
+++ resources/views/components/competence/item/v1.blade.php
@@ -0,0 +1,6 @@
+<li class="flex items-center flex-col gap-y-4 w-16 lg:w-32 min-w-16 lg:min-w-32">
+    {{ $icon }}
+    <h4 class="text-[0.5rem] lg:text-base font-bold text-center">
+        {{ $title }}
+    </h4>
+</li>

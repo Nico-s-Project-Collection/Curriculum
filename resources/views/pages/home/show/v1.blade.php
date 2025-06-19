503c278319a4e961b7978344ba51eabefab0183c
Nicolas R. Lopes <nicolasrlopes48@gmail.com>
2025-06-19 19:03:17 -0300

    feat: add figma in ux and ui design competence group software row in home

--- resources/views/pages/home/show/v1.blade.php
+++ resources/views/pages/home/show/v1.blade.php
@@ -12,6 +12,14 @@
                     </x-slot>
                     <x-slot name="content">
                         <x-competence.h-scroll.v1>
+                            <x-competence.item.v1>
+                                <x-slot name="icon">
+                                    <i class="fa-brands fa-figma text-[3rem] lg:text-[6rem]" aria-hidden="true"></i>
+                                </x-slot>
+                                <x-slot name="title">
+                                    Figma
+                                </x-slot>
+                            </x-competence.item.v1>
                         </x-competence.h-scroll.v1>
                     </x-slot>
                 </x-competence.container.v1>

ea7f92caa68f0a3a6a23f147e5057ae23d4ac190
Nicolas R. Lopes <nicolasrlopes48@gmail.com>
2025-06-29 19:16:56 -0300

    feat: add laravel in back-end competence group frameworks row in home

--- resources/views/pages/home/show/v1.blade.php
+++ resources/views/pages/home/show/v1.blade.php
@@ -195,6 +195,14 @@
                             </x-competence.item.v1>
                         </x-competence.h-scroll.v1>
                         <x-competence.h-scroll.v1>
+                            <x-competence.item.v1>
+                                <x-slot name="icon">
+                                    <i class="fa-brands fa-laravel text-[3rem] lg:text-[6rem]" aria-hidden="true"></i>
+                                </x-slot>
+                                <x-slot name="title">
+                                    Laravel
+                                </x-slot>
+                            </x-competence.item.v1>
                         </x-competence.h-scroll.v1>
                     </x-slot>
                 </x-competence.container.v1>

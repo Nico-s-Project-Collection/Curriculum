496b990e3b1a6d6517b7b9ca8f383b34f1290b97
Nicolas R. Lopes <nicolasrlopes48@gmail.com>
2025-07-05 22:45:37 -0300

    fix: center competence item second version icon

--- resources/views/components/competence/item/v2.blade.php
+++ resources/views/components/competence/item/v2.blade.php
@@ -1,5 +1,5 @@
-<li class="flex items-center flex-col gap-y-4 w-16 lg:w-32 min-w-16 lg:min-w-32">
-    <a class="block size-full" href="{{ $href }}">
+<li class="w-16 lg:w-32 min-w-16 lg:min-w-32">
+    <a class="flex items-center flex-col gap-y-4 size-full" href="{{ $href }}">
         {{ $icon }}
         <h4 class="text-[0.5rem] lg:text-base font-bold text-center">
             {{ $title }}

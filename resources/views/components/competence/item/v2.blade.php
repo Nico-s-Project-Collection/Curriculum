f35094ef1cce8f5d093e75ceb89f17f43c1f7da6
Nicolas R. Lopes <nicolasrlopes48@gmail.com>
2025-07-05 22:41:37 -0300

    feat: add competence item second variation

---
+++ resources/views/components/competence/item/v2.blade.php
@@ -0,0 +1,8 @@
+<li class="flex items-center flex-col gap-y-4 w-16 lg:w-32 min-w-16 lg:min-w-32">
+    <a class="block size-full" href="{{ $href }}">
+        {{ $icon }}
+        <h4 class="text-[0.5rem] lg:text-base font-bold text-center">
+            {{ $title }}
+        </h4>
+    </a>
+</li>

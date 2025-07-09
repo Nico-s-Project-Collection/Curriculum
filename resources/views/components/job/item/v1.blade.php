545b4d70b802b17d9ccf123994183d52adbec2b1
Nicolas R. Lopes <nicolasrlopes48@gmail.com>
2025-07-08 23:40:10 -0300

    feat: add item job component

---
+++ resources/views/components/job/item/v1.blade.php
@@ -0,0 +1,15 @@
+<li class="flex justify-between">
+    <div class="flex flex-col gap-y-4">
+        <h1 class="text-[2rem] font-bold leading-none">
+            {{ $position }}
+        </h1>
+        <h2 class="text-[1.75rem] font-medium leading-none">
+            {{ $enterprise }}
+        </h2>
+    </div>
+    <div>
+        <p class="text-[2rem] font-medium leading-none">
+            {{ $period }}
+        </p>
+    </div>
+</li>

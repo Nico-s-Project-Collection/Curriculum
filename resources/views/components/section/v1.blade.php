fb6d4ff7d55196344b9b99c8b9c3c47c40b5faa0
Nicolas R. Lopes <nicolasrlopes48@gmail.com>
2025-07-10 21:15:43 -0300

    feat: hide section if no content is set

--- resources/views/components/section/v1.blade.php
+++ resources/views/components/section/v1.blade.php
@@ -1,8 +1,15 @@
-<section class="flex flex-col gap-y-8 lg:gap-y-16">
-    <h1 class="text-2xl lg:text-[3rem] font-semibold leading-none px-4 lg:px-8">
-        {{ $title }}
-    </h1>
-    <div class="flex flex-col gap-y-8 lg:gap-y-16">
-        {{ $content }}
-    </div>
-</section>
+@props([
+    'title'   => '',
+    'content' => '',
+])
+
+@if ($content != '')
+    <section class="flex flex-col gap-y-8 lg:gap-y-16">
+        <h1 class="text-2xl lg:text-[3rem] font-semibold leading-none px-4 lg:px-8">
+            {{ $title }}
+        </h1>
+        <div class="flex flex-col gap-y-8 lg:gap-y-16">
+            {{ $content }}
+        </div>
+    </section>
+@endif

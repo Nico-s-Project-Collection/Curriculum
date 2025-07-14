9ced69b72c9a9824436daf23e8aac9cc9fe61a18
Nicolas R. Lopes <nicolasrlopes48@gmail.com>
2025-07-13 23:57:56 -0300

    feat: show h-scroll item icon placeholder if no icon is set

--- resources/views/components/h-scroll/item/v1.blade.php
+++ resources/views/components/h-scroll/item/v1.blade.php
@@ -1,5 +1,14 @@
+@props([
+    'icon'  => '',
+    'title' => ''
+])
+
 <li class="flex items-center flex-col gap-y-4 w-16 lg:w-32 min-w-16 lg:min-w-32">
-    {{ $icon }}
+    @if ($icon != '')
+        {{ $icon }}
+    @else
+        <div class="bg-gray-300 h-32 w-24"></div>
+    @endif
     <h4 class="text-[0.5rem] lg:text-base font-bold text-center">
         {{ $title }}
     </h4>

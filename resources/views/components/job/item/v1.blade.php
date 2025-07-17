f71277ddaaffa50134d150ece2aead3bd0306284
Nicolas R. Lopes <nicolasrlopes48@gmail.com>
2025-07-17 20:52:38 -0300

    style: decrease job item first version font size

--- resources/views/components/job/item/v1.blade.php
+++ resources/views/components/job/item/v1.blade.php
@@ -1,14 +1,14 @@
 <li class="flex justify-between">
     <div class="flex flex-col gap-y-4">
-        <h1 class="text-[2rem] font-bold leading-none">
+        <h1 class="text-[1.5rem] font-bold leading-none">
             {{ $position }}
         </h1>
-        <h2 class="text-[1.75rem] font-medium leading-none">
+        <h2 class="text-[1.25rem] font-medium leading-none">
             {{ $enterprise }}
         </h2>
     </div>
     <div>
-        <p class="text-[2rem] font-medium leading-none">
+        <p class="font-mono text-[1.5rem] font-medium leading-none">
             {{ $period }}
         </p>
     </div>
